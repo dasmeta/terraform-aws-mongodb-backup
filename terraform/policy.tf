a694b789938c1bb0582c50d0e901ea99da35064c
Mher Poghosyan <devops.mher@gmail.com>
2021-09-29 18:12:41 +0400

    update module

--- terraform/policy.tf
+++ terraform/policy.tf
@@ -1,6 +1,9 @@
 resource "aws_iam_user_policy" "iam_user_policy" {
   name = "s3-buckert-mongo"
   user = module.iam_user.iam_user_name
+  depends_on = [
+      module.iam_user
+    ]
   policy = jsonencode({
     "Version": "2012-10-17",
     "Statement": [
