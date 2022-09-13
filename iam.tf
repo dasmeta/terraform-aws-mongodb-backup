37d799ef9bd7f20048de44f3715615ef24d33ca1
Tigran Muradyan <tigran@dasmeta.com>
2022-09-13 13:26:56 +0400

    DMVP-1086 fix mongodb backup iam policy

--- iam.tf
+++ iam.tf
@@ -1,15 +1,18 @@
 module "mongodb_backup_s3_storage_user" {
-  source          = "dasmeta/modules/aws//modules/aws-iam-user"
-  create_user     = var.create_user
-  username        = var.backup_user_name
-  console         = false
-  policy          = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+  source  = "dasmeta/modules/aws//modules/aws-iam-user"
+  version = "0.36.8"
+
+  create_user   = var.create_user
+  username      = var.backup_user_name
+  console       = false
+  create_policy = true
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Effect": "Allow",
-        "Action": "s3:PutObject",
-        "Resource": "arn:aws:s3:::${var.s3_bucket}/*"
+        "Effect" : "Allow",
+        "Action" : "s3:PutObject",
+        "Resource" : "arn:aws:s3:::${var.s3_bucket}/*"
       }
     ]
   })
